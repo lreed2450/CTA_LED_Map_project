c6d69e40e30e3fea6069dc85554c03cb20f0f309
lreed2450 <lexi.reed028@gmail.com>
2026-02-28 23:46:28 -0600

    first write to db

--- create_schema_and_tables.sql
+++ create_schema_and_tables.sql
@@ -1,27 +1,27 @@
 CREATE SCHEMA raw;
 
-CREATE TABLE raw.src_stop_metadata(  
-    stop_id                     INT NOT NULL PRIMARY KEY,
-    direction_id                VARCHAR(1),
-    stop_name                   VARCHAR(30),
-    station_name                VARCHAR(30),
-    station_descriptive_name    VARCHAR(40),
-    map_id                      INT,
-    ada                         BOOLEAN,
-    red                         BOOLEAN,
-    blue                        BOOLEAN,
-    g                           BOOLEAN,
-    brn                         BOOLEAN,
-    p                           BOOLEAN,
-    y                           BOOLEAN,
-    pnk                         BOOLEAN,
-    o                           BOOLEAN,
-    location                    VARCHAR(40)
+CREATE TABLE IF NOT EXISTS raw.src_stop_metadata(  
+    STOP_ID                     INT NOT NULL PRIMARY KEY,
+    DIRECTION_ID                VARCHAR(1),
+    STOP_NAME                   VARCHAR(30),
+    STATION_NAME                VARCHAR(30),
+    STATION_DESCRIPTIVE_NAME    VARCHAR(40),
+    MAP_ID                      INT,
+    ADA                         BOOLEAN,
+    RED                         BOOLEAN,
+    BLUE                        BOOLEAN,
+    G                          BOOLEAN,
+    BRN                         BOOLEAN,
+    P                           BOOLEAN,
+    Y                           BOOLEAN,
+    Pnk                         BOOLEAN,
+    O                           BOOLEAN,
+    Location                    VARCHAR(40)
 );
 
 
 -- all data from lapi initially loads as strings
-CREATE TABLE raw.src_lapi_data(  
+CREATE TABLE IF NOT EXISTS raw.src_lapi_data(  
 
     id                          SERIAL PRIMARY KEY,
     next_station_parent_id      VARCHAR(10),
